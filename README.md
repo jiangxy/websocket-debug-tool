5e6813be136db2b10c3d0f2a6f220d9a04be67d0
jiangxiyang <jiangxiyang@weidian.com>
2017-04-17 19:11:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# websocket debug tool
+
+A tool to debug websocket/sockjs/stomp.
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebSocketDebugTool from './components/WebSocketDebugTool';
+
+ReactDOM.render(<WebSocketDebugTool/>, document.getElementById('root'));
