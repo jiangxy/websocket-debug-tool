740c5304014db9c6ed86c721a1e8db92486fa83d
jiangxiyang <jiangxiyang@weidian.com>
2017-04-17 19:50:17 +0800

    update prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,5 @@
 const webpack = require('webpack');
-const path = require('path');
-const globalConfig = require('./src/config.js');
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -11,18 +8,6 @@ const babelLoaderConfig = {
   plugins: [['import', {libraryName: 'antd', style: true}]],
   cacheDirectory: true,
 };
-
-const lessLoaderVars = {
-  sidebarCollapsible: globalConfig.sidebar.collapsible,
-};
-
-// bundle split, 尝试把这些比较独立的库单独放在一个js文件中
-// 注意只有真正"公用"的库才能放这里, 否则会有各种奇怪的问题
-// 而且必须在bundle.js之前加载
-const vendorLibs = ['react', 'react-router',
-  'redux', 'react-redux', 'redux-logger', 'redux-thunk', 'redux-promise',
-  'superagent',
-];
 
 module.exports = {
   devtool: 'cheap-module-source-map',
@@ -35,7 +20,6 @@ module.exports = {
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.min.js',
-    // publicPath: 'http://mycdn.com/', // require时用来生成图片的地址
   },
 
   resolve: {
@@ -52,12 +36,10 @@ module.exports = {
         exclude: /node_modules/,
       }, {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css'),
-        //loader: 'style!css',
+        loader: 'style!css',
       }, {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style', 'css!' + `less?{"sourceMap":true,"modifyVars":${JSON.stringify(lessLoaderVars)}}`),
-        //loader: 'style!css!' + `less?{"sourceMap":true,"modifyVars":${JSON.stringify(lessLoaderVars)}}`,
+        loader: 'style!css!' + `less?{"sourceMap":true}`,
       }, {
         test: /\.(png|jpg|svg)$/,
         loader: 'url?limit=25000',
@@ -82,31 +64,10 @@ module.exports = {
 
     new HtmlWebpackPlugin({
       template: 'index.html.template',
-      title: globalConfig.name,
-      favIcon: globalConfig.favicon,
+      title: 'WebSocket Debug Tool',
+      favIcon: 'http://jxy.me/favicon.ico',
       hash: true,  // 引入js/css的时候加个hash, 防止cdn的缓存问题
       minify: {removeComments: true, collapseWhitespace: true},
-    }),
-
-    // 抽离公共部分, 要了解CommonsChunkPlugin的原理, 首先要搞清楚chunk的概念
-    // CommonsChunkPlugin做的其实就是把公共模块抽出来, 可以单独生成一个新的文件, 也可以附加到已有的chunk上
-    // 同时还会加上webpack的runtime相关代码
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.min.js',
-      // 这个函数决定哪些模块会被放到vender.min.js中
-      minChunks: (module) => {
-        // 得到资源路径
-        var resource = module.resource;
-        if (!resource)
-          return false;
-        // 坑爹的webpack, for-of里不用能const, 会有bug
-        for (var libName of vendorLibs) {
-          if (resource.indexOf(path.resolve(__dirname, 'node_modules', libName)) >= 0)
-            return true;
-        }
-        return false;
-      },
     }),
 
     new webpack.optimize.DedupePlugin(),
@@ -116,8 +77,6 @@ module.exports = {
     // 允许错误不打断程序
     new webpack.NoErrorsPlugin(),
 
-    // css单独抽出来
-    new ExtractTextPlugin('bundle.min.css', {allChunks: false}),
     // 压缩成gzip格式
     new CompressionPlugin({
       asset: "[path].gz[query]",
