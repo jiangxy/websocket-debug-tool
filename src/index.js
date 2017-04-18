6d22746184bd77a63d4a06c1930bf0aea2657e63
jiangxiyang <jiangxiyang@weidian.com>
2017-04-18 11:15:18 +0800

    add gh-pages

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WebSocketDebugTool from './components/WebSocketDebugTool';
-
-ReactDOM.render(<WebSocketDebugTool/>, document.getElementById('root'));
